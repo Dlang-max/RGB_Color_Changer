a646b05713bcb2fa0ae9c51ee0ea6916b1101f03
Dlang-max <langd052405@gmail.com>
2025-05-21 20:10:52 -0400

    created file for changing rgb colors

---
+++ change_rgb.c
@@ -0,0 +1,15 @@
+#include <libusb-1.0/libusb.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#define VENDOR_ID 0x046d
+#define PRODUCT_ID 0xc335
+
+int main(int argc, char *argv[]) {
+
+	// Initialize connection to usb device
+	// Send USB HID packet containing color HEX
+	
+	return 0;
+}
